68074bfb65ec05ce6ce687848a8365dd393428ed
Vuillemot Florian <vuillemot.florian@gmail.com>
2019-02-04 14:11:27 +0100

    Add entrypoint

---
+++ front/entrypoint/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calendar from './calendar/Calendar.js';
+import SelectionAccountingInvoicing from './selectionAccountingInvoicing/selectionAccountingInvoicing.js';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      year: null,
+      strMonth: null
+    }
+
+    this.handleClick = this.handleClick.bind(this); 
+    this.clear = this.clear.bind(this); 
+  }
+
+  clear() {
+    this.setState({
+      year: null,
+      strMonth: null
+    });
+  }
+
+  handleClick(year, strMonth){
+    this.setState({
+      year: year,
+      strMonth: strMonth
+    });
+    console.log(year, strMonth);
+  }
+
+  getBalise(){
+    if (this.state.year === null){
+      return <Calendar onClick={this.handleClick} />
+    }
+    return <SelectionAccountingInvoicing
+                isInvoicing={() => console.log("invoice")}
+                isAccounting={() => console.log("account")}
+                back={() => this.clear()}
+            />
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          {this.getBalise()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front/entrypoint/src/calendar/Calendar.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+
+class Calendar extends Component {
+    constructor(props){
+        super(props);
+
+        const today = new Date();
+        this.state = {
+            year: today.getFullYear(),
+            month: this.months()[today.getMonth()]
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick() {
+        const year = parseInt(this.state.year);
+        const monthExist = this.months().find(m => m === this.state.month);
+        
+        if (2010 <= year && year < 2100 && monthExist){
+            this.props.onClick(this.state.year, this.state.month);
+        }
+        else {
+            alert('Date incorrect')
+        }
+    }
+    months() {
+        return ['Janvier', 'Février', 'Mars', 'Avril', 'Mais', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+    }
+
+    getYears() {
+        return [...Array(100)].map((_, i) => i)
+                              .filter(i => i > 9)
+                              .map(i => `20${i}`)
+    }
+    
+    render(){
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Année</th>
+                                <th>Mois</th>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <input
+                                        name="year"
+                                        list="years"
+                                        defaultValue={this.state.year}
+                                        onChange={(e) => this.setState({'year': e.target.value})}
+                                    />
+                                </td>
+                                <td>
+                                    <input
+                                        name="month"
+                                        list="months"
+                                        defaultValue={this.state.month}
+                                        onChange={(e) => this.setState({'month': e.target.value})}
+                                    />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colSpan="2">
+                                    <button
+                                        type="button"
+                                        onClick={this.handleClick}
+                                    >
+                                        Valider
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <datalist id="years">
+                    {this.getYears().map(i => <option value={i} key={i}/>)}
+                </datalist>
+                <datalist id="months">
+                    {this.months().map(m => <option value={m} key={m}/>)}
+                </datalist>
+            </div>
+        );
+    }
+}
+
+
+export default Calendar;
