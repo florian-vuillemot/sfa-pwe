1827227ec871e12d3520b50933d3680a27e12744
Vuillemot Florian <vuillemot.florian@gmail.com>
2019-02-06 17:20:18 +0100

    Cleanning

--- front/invoicing/src/App.js
+++ front/invoicing/src/App.js
@@ -81,97 +81,53 @@ class WorkingDays extends Component {
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 function GlobalInfo(props){
-  const onConstructionSiteChange = (e) => props.onConstructionSiteChange(e.target.name, e.target.value);
   const info = props.constructionSiteInfo;
   const rate = info.rate;
+
+  const onConstructionSiteChange = (e) => props.onConstructionSiteChange(e.target.name, e.target.value);
+  const inputNumber = (name, text, colSpan) => {
+    return (
+      <th colSpan={colSpan}>
+        <label forhtml={name}>{text}</label>
+        <input type="number" name={name} step="any" defaultValue={rate[name]} onChange={onConstructionSiteChange}/>
+      </th>
+    );
+  }
 
   return (
     <thead>
       <tr>
         <th>
           <label forhtml="client">Client </label>
-          <input
-            name="client"
-            list="clients"
-            defaultValue={info.client}
-            onChange={onConstructionSiteChange}
-          />
-          <datalist id="clients">
-            <option value="Eurovia"/>
-            <option value="Gille"/>
-          </datalist>
+          <input name="client" list="clients" defaultValue={info.client} onChange={onConstructionSiteChange} onBlur={onConstructionSiteChange}/>
+          <datalist id="clients">{props.clients.map(c => <option value={c} key={c}/>)}</datalist>
         </th>
-        <th colSpan="5">
-          <label forhtml="hourTaxFreePrice">Prix horaire HT </label>
-          <input
-            name="hourTaxFreePrice"
-            type="number"
-            step="any"
-            defaultValue={rate.hourTaxFreePrice}
-            onChange={onConstructionSiteChange}
-          />
-        </th>
+        {inputNumber("hourTaxFreePrice", "Prix horaire HT ", 5)}
       </tr>
       <tr>
         <th>
           <label forhtml="place">Lieu </label>
-          <input
-            type="text"
-            name="place"
-            defaultValue={info.place}
-            onChange={onConstructionSiteChange}
-          />
+          <input type="text" name="place" defaultValue={info.place} onChange={onConstructionSiteChange}/>
         </th>
-        <th colSpan="5">
-          <label forhtml="dayTaxFreePrice">Prix journalier HT </label>
-          <input
-            type="number"
-            name="dayTaxFreePrice"
-            step="any"
-            defaultValue={rate.dayTaxFreePrice}
-            onChange={onConstructionSiteChange}
-          />
-        </th>
+        {inputNumber("dayTaxFreePrice", "Prix journalier HT ", 5)}
       </tr>
       <tr>
-        <th>
-        </th>
-        <th colSpan="6">
-          <label forhtml="taxPercent">TVA (en %) </label>
-          <input
-            type="number"
-            name="taxPercent"
-            step="any"
-            defaultValue={rate.taxPercent}
-            onChange={onConstructionSiteChange}
-          />
-        </th>
+        <th></th>
+        {inputNumber("taxPercent", "TVA (en %) ", 6)}
       </tr>
     </thead>
-  );
-}
-
-function TableDay(props){
-  return (
-    <tr>
-      <th>Date</th>
-      <th>Type</th>
-      <th>Heures</th>
-      <th>Prix HT</th>
-      <th>Prix TTC</th>
-      <th></th>
-    </tr>
   );
 }
 
 function DayInfo(props){
   const day = props.day;
   const onDayChange = (event) => props.onDayChange(day, event.target.name, event.target.value);
+  const inputNumber = (name) => <input type="number" name={name} step="any" defaultValue={day[name]} onChange={onDayChange}/>;
 
   return (
     <tr>
       <td>
-        <p><input type="date" name="date" defaultValue={day.date} onChange={onDayChange}/></p>
+        <input type="date" name="date" defaultValue={day.date} onChange={onDayChange}/>
       </td>
       <td>
         <p>
@@ -183,21 +139,16 @@ function DayInfo(props){
         </p>
       </td>
       <td>
-        <p>
-          {day.type === "HOURS" ? <input type="number" name="hours" step="any" defaultValue={day.hours} onChange={onDayChange}/> : null}
-        </p>
+        <p>{day.type === "HOURS" ? inputNumber("hours") : null}</p>
       </td>
       <td>
-        {day.type === "TRANSFER" ?
-          <p><input type="number" name="taxFreePrice" step="any" defaultValue={day.taxFreePrice} onChange={onDayChange}/></p>
-          : <p>{day.taxFreePrice}</p>
-        }
+        <p>{day.type === "TRANSFER" ? inputNumber("taxFreePrice") : day.taxFreePrice}</p>
       </td>
       <td>
         <p>{day.price}</p>
       </td>
       <td>
-        <i className="fa fa-trash" onClick={(_) => props.deleteDay(day)}></i>
+        <i className="fa fa-trash" onClick={() => props.deleteDay(day)}></i>
       </td>
     </tr>
   );
@@ -214,29 +165,38 @@ function newDay(){
 }
 
 function Day(props){
-  const newDayNeed = (daysSorted) => {
-    const len = daysSorted.length;
-    const day = newDay();
-
-    return len === 0 || !daysSorted.find(d => !d.date) ? <DayInfo day={day} key={day.id} onDayChange={props.onDayChange} deleteDay={props.deleteDay}/> : null;
-  };
   const daysSorted = props.days.sort((d1, d2) => d1.date ? d1.date < d2.date : true);
   const days = daysSorted.map(d => <DayInfo day={d} key={d.id} onDayChange={props.onDayChange} deleteDay={props.deleteDay}/>)
 
+  const newDayNeed = (daysSorted) => {
+    const len = daysSorted.length;
+    const day = newDay();
+    const createDay = len === 0 || !daysSorted.find(d => !d.date);
+    return createDay ? <DayInfo day={day} key={day.id} onDayChange={props.onDayChange} deleteDay={props.deleteDay}/> : null;
+  };
+
   return (
     <tbody>
-      <TableDay />
+      <tr>
+        <th>Date</th>
+        <th>Type</th>
+        <th>Heures</th>
+        <th>Prix HT</th>
+        <th>Prix TTC</th>
+        <th></th>
+      </tr>
       {days}
       {newDayNeed(daysSorted)}
     </tbody>
   )
 }
 
-class WorkingDay extends Component{
+class ConstructionSite extends Component{
   constructor(props){
     super(props);
 
     this.state = {
+      clients: ["Eurovia", "Gille", "Paul"],
       data: {
         constructionSiteInfo: {
           client: "Eurovia",
@@ -270,63 +230,39 @@ class WorkingDay extends Component{
     this.onConstructionSiteChange = this.onConstructionSiteChange.bind(this);
   }
 
-  taxFreePrice(day, transferTaxFreePrice = null) {
+  calculPrices(day, transferTaxFreePrice = null) {
     const data = this.state.data;
-    const tax = data.constructionSiteInfo.rate.taxPercent / 100;
+    const rate = data.constructionSiteInfo.rate;
+    const tax = rate.taxPercent / 100;
 
     if (day.hours !== null && day.hours !== undefined) {
-      const hoursTaxFree = data.constructionSiteInfo.rate.hourTaxFreePrice * 100;
-      const priceTaxFree = hoursTaxFree * day.hours;
-      const price = priceTaxFree + priceTaxFree * tax;
-      return {...day, taxFreePrice: priceTaxFree / 100, price: price / 100};
+      return {...day, ...hoursPrice(day.hours, rate.hourTaxFreePrice, tax)};
     }
-    
-    let operationPrice = null;
-    if (day.type === "TRANSFER")
-      operationPrice = transferTaxFreePrice ? transferTaxFreePrice : day.taxFreePrice;
-    else
-      operationPrice = data.constructionSiteInfo.rate.dayTaxFreePrice;
-    operationPrice *= 100;
-
-    const price = operationPrice + operationPrice * tax;
-    return {...day, taxFreePrice: operationPrice / 100, price: price / 100};
+    if (day.type === "TRANSFER") {
+      return {...day, ...getPrice(transferTaxFreePrice ? transferTaxFreePrice : day.taxFreePrice, tax)};
+    }
+    return {...day, ...getPrice(rate.dayTaxFreePrice, tax)};
   }
 
   onDayChange(day, name, value) {
-    const fcts = [
+    const dayChangeOperations = [
       (day, name, value) => name === "date" ? {...day, date: value} : day,
-      (day, name, value) => {
-        if (name === 'type') {
-          if (value === 'HOURS') {
-            return this.taxFreePrice({...day, hours: 0.0, type: value})
-          }
-          return this.taxFreePrice({...day, hours: null, type: value})
-        }
-        return day;
-      },
-      (day, name, value) => {
-        if (name === 'hours'){
-          return this.taxFreePrice({...day, hours: value})
-        }
-        return day;
-      },
-      (day, name, value) => {
-        if (name === 'taxFreePrice') {
-          return this.taxFreePrice(day, value)
-        }
-        return day;
-      }
+      (day, name, value) => name === 'hours' ? this.calculPrices({...day, hours: value}) : day,
+      (day, name, value) => name === 'taxFreePrice' ? this.calculPrices(day, value) : day,
+      (day, name, value) => name === 'type' ? this.calculPrices({...day, hours: value === 'HOURS' ? 0.0 : null, type: value}) : day
     ];
-    const workingDays = this.state.data.workingDays.filter(d => d.id !== day.id);
-    const newWorkingDays = fcts.reduce((d, fct) => fct(d, name, value), day);
-    const data = {...this.state.data, workingDays: [...workingDays, newWorkingDays]}
-    this.setState({data: data});
+    const data = this.state.data;
+    const workingDays = data.workingDays.filter(d => d.id !== day.id);
+    const newWorkingDays = dayChangeOperations.reduce((d, fct) => fct(d, name, value), day);
+    const newData = {...data, workingDays: [...workingDays, newWorkingDays]}
+    this.setState({data: newData});
   }
 
   deleteDay(day) {
-    const workingDays = this.state.data.workingDays.filter(d => d.id !== day.id);
-    const data = {...this.state.data, workingDays: workingDays}
-    this.setState({data: data});
+    const data = this.state.data;
+    const workingDays = data.workingDays.filter(d => d.id !== day.id);
+    const newData = {...data, workingDays: workingDays}
+    this.setState({data: newData});
   }
 
   onConstructionSiteChange(name, value) {
@@ -343,8 +279,8 @@ class WorkingDay extends Component{
       const newConstructionSiteInfo = {...constructionSiteInfo, rate: newRate};
 
       this.setState({data: {...data, constructionSiteInfo: newConstructionSiteInfo}}, () => {
-        const workingDays = this.state.data.workingDays.map(d => this.taxFreePrice(d))
-        this.setState({data: {...this.state.data, workingDays: workingDays}});
+        const workingDays = this.state.data.workingDays.map(d => this.calculPrices(d))
+        this.setState({data: {...this.state.data, workingDays: workingDays}});      
       });
     }
   }
@@ -353,6 +289,7 @@ class WorkingDay extends Component{
     return (
       <table className="Working-Days">
         <GlobalInfo
+          clients={this.state.clients}
           constructionSiteInfo={this.state.data.constructionSiteInfo}
           onConstructionSiteChange={this.onConstructionSiteChange}
         />
@@ -366,6 +303,18 @@ class WorkingDay extends Component{
   }
 }
 
+function hoursPrice(hours, hourTaxFreePrice, tax) {
+  const hoursTaxFree = hourTaxFreePrice * 100;
+  const priceTaxFree = hoursTaxFree * hours;
+  const price = priceTaxFree + priceTaxFree * tax;
+  return {taxFreePrice: priceTaxFree / 100, price: price / 100}
+}
+
+function getPrice(taxFreePrice, tax){
+  const intTaxFreePrice = taxFreePrice * 100;
+  const price = intTaxFreePrice + intTaxFreePrice * tax;
+  return {taxFreePrice: taxFreePrice, price: price / 100};
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -396,7 +345,7 @@ class App extends Component {
         <p>Menu principal</p>
         <header className="App-header">
           {//this.state.onNewWorkDay ?
-            <WorkingDay />// : <WorkingDays onNewWorkDay={this.newWorkDay}/>
+            <ConstructionSite />// : <WorkingDays onNewWorkDay={this.newWorkDay}/>
           }
         </header>
       </div>
