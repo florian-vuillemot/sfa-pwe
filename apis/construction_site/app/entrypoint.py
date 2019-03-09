58884a917c5bca29b5aac37a8975ecc71ee294d6
Vuillemot Florian <florian.vuillemot@posteo.net>
2019-03-09 19:39:48 +0100

    Construction site entrypoint

--- apis/construction_site/app/entrypoint.py
+++ apis/construction_site/app/entrypoint.py
@@ -5,5 +5,19 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/constructions-site")
+def constructions_site():
+    db_connector
+    return "All constructions site"
+
+@app.route("/construction-site", methods=['POST'])
+@app.route("/construction-site/<id>", methods=['GET', 'PUT'])
+def construction_site(id=None):
+    if request.method == 'POST':
+        return "post"
+    if request.method == 'GET':
+        return "get"
+    return "put"
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
